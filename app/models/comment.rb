f1c0188490c24ae241b16f50ec3110e3a189ca1b
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-29 17:23:51 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+
   def user_attributes=(user_attributes)
-    user_attributes.values.each do |user_attributes|
-      user = User.find_or_create_by(user_attributes)
-      self.user << user
-    end
+    self.user = User.find_or_create_by(username: user_attributes[:username])
   end
+  # def user_attributes=(user_attributes)
+  #   user_attributes.values.each do |user_attributes|
+  #     user = User.find_or_create_by(user_attributes)
+  #     self.user << user
+  #   end
+  # end
 end
